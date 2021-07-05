898bbb63567067a44d5c685155f3c3b7d26a2dd2
Pablo Jorda <pjorda96@gmail.com>
2021-07-05 10:40:48 +0200

    text bubble

---
+++ src/components/TextBubble/TextBubble.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextBubble as TextBubbleStyled } from './TextBubbleStyles';
+
+export default function TextBubble() {
+  return (
+    <TextBubbleStyled>Hey there. I'm using whatsapp</TextBubbleStyled>
+  )
+}
